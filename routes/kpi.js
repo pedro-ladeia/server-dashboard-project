5c1f4154b0789eb7693d2f90becda7821ae19bbb
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-04-24 22:02:48 -0300

    Route adjusted

--- routes/kpi.js
+++ routes/kpi.js
@@ -4,7 +4,7 @@ import KPI from "../models/KPI.js"
 
 const router = express.Router();
 
-router.get("/kpi", async (req, res) =>  {
+router.get("",async (req, res) =>  {
     try{
         const kpis = await KPI.find();
         res.status(200).json(kpis);
